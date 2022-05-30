a2fe3de93802affa589803b69df24394211f44e2
uyamorelia <104292036+uyamorelia@users.noreply.github.com>
2022-05-29 16:16:25 -0800

    Add files via upload

---
+++ js/gscript.js
@@ -0,0 +1,155 @@
+
+      
+      var script_url = "https://script.google.com/macros/s/AKfycby4KLrRoJIk70Q_wyNTEsNq-4roG6myaNxTMxICy1WDekYFASRGug9z6MrSFpkwzJ2y/exec";
+      
+      // Make an AJAX call to Google Script
+      function insert_value() {
+        
+        $("#re").css("visibility","hidden");
+         document.getElementById("loader").style.visibility = "visible";
+        $('#mySpinner').addClass('spinner');
+    
+    var id1=	$("#id").val();
+        var name= $("#name").val();
+        var weburl= $("#weburl").val();
+        var supurl= $("#supurl").val();
+        var va= $("#va").val();
+        
+        
+        var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&weburl="+weburl+"&supurl="+supurl+"&va="+va+"&action=insert";
+        
+    
+        var request = jQuery.ajax({
+          crossDomain: true,
+          url: url ,
+          method: "GET",
+          dataType: "jsonp"
+        });
+    
+      }
+    
+    
+      
+      
+     
+      
+      
+      function update_value(){
+        $("#re").css("visibility","hidden");
+         document.getElementById("loader").style.visibility = "visible";
+        
+        
+    var id1=	$("#id").val();
+        var name= $("#name").val();
+        var weburl= $("#weburl").val();
+        var supurl= $("#supurl").val();
+        var va= $("#va").val();
+        
+        var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&weburl="+weburl+"&supurl="+supurl+"&va="+va+"&action=update";
+        
+    
+        var request = jQuery.ajax({
+          crossDomain: true,
+          url: url ,
+          method: "GET",
+          dataType: "jsonp"
+        });
+    
+        
+      }
+    
+      
+     
+      
+      
+      function delete_value(){
+        $("#re").css("visibility","hidden");
+         document.getElementById("loader").style.visibility = "visible";
+        $('#mySpinner').addClass('spinner');
+    var id1=	$("#id").val();
+        var name= $("#name").val();
+        var weburl= $("#weburl").val();
+        var supurl= $("#supurl").val();
+        var va= $("#va").val();
+        
+        
+        var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&weburl="+weburl+"&supurl="+supurl+"&va="+va+"&action=delete";
+        
+    
+        var request = jQuery.ajax({
+          crossDomain: true,
+          url: url ,
+          method: "GET",
+          dataType: "jsonp"
+        });
+    
+      }
+    
+    
+      
+      
+      // print the returned data
+      function ctrlq(e) {
+      
+        
+        $("#re").html(e.result);
+        $("#re").css("visibility","visible");
+        read_value();
+        
+      }
+      
+      
+    
+      
+    function read_value() {
+    
+    $("#re").css("visibility","hidden");
+       
+       document.getElementById("loader").style.visibility = "visible";
+     var url = script_url+"?action=read";
+    
+    $.getJSON(url, function (json) {
+    
+        // Set the variables from the results array
+       
+       
+      
+    
+            // CREATE DYNAMIC TABLE.
+            var table = document.createElement("table");
+    
+            
+    
+            var header = table.createTHead();
+            var row = header.insertRow(0);     
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+        
+            cell1.innerHTML = "<b>Asin</b>";
+            cell2.innerHTML = "<b>Store Name</b>";
+            cell3.innerHTML = "<b>Time stamp</b>";
+            
+            
+            // ADD JSON DATA TO THE TABLE AS ROWS.
+            for (var i = 0; i < json.records.length; i++) {
+    
+                tr = table.insertRow(-1);
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = json.records[i].ID;
+                    tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = json.records[i].NAME;
+                    tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = json.records[i].Time_stamp;
+                    tabCell = tr.insertCell(-1);
+                }
+          
+    
+            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+            var divContainer = document.getElementById("showData");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
+            document.getElementById("loader").style.visibility = "hidden";
+            $("#re").css("visibility","visible");
+        });
+        }
---
+++ js/reloadbutton.js
@@ -0,0 +1,10 @@
+
+        const reloadtButton = document.querySelector("#reload");
+        // Reload everything:
+        function reload() {
+        reload = location.reload();
+                          }
+        // Event listeners for reload
+        reloadButton.addEventListener("click", reload, false);
+        
+        
