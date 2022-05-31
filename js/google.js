ed39e575876f4bf5cdb27bf5fb6812119bb97f15
eidxdie <christian1208bayani@gmail.com>
2022-05-30 23:46:24 -0800

    Update google.js

--- js/google.js
+++ js/google.js
@@ -120,8 +120,8 @@ $.getJSON(url, function (json) {
 		var row = header.insertRow(0);     
 		var cell1 = row.insertCell(0);
 		var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
         
         cell1.innerHTML = "<b>Asin</b>";
         cell2.innerHTML = "<b>Store Name</b>";
