3d8b106853515e5da89e38e2461c4de87ce98298
eidxdie <christian1208bayani@gmail.com>
2022-05-31 06:45:51 -0800

    Update google.js

--- js/google.js
+++ js/google.js
@@ -119,30 +119,15 @@ $.getJSON(url, function (json) {
         var header = table.createTHead();
 		var row = header.insertRow(0);     
 		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
+		
         
-        cell1.innerHTML = "<b>Asin</b>";
-        cell2.innerHTML = "<b>Store Name</b>";
-        cell3.innerHTML = "<b>VA NAME</b>";
-        cell4.innerHTML = "<b>Time stamp</b>";
+        cell1.innerHTML = "<b></b>";
+      
+        
         
         // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < json.records.length; i++) {
-
-            tr = table.insertRow(-1);
-				var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = json.records[i].UPC;
-				tabCell = tr.insertCell(-1);
-				tabCell.innerHTML = json.records[i].CLUSTER;
-                tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = json.records[i].VA_NAME;
-                tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = json.records[i].TIME_STAMP;
-                    tabCell = tr.insertCell(-1);
+        
                 
-            }
       
 
         // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
